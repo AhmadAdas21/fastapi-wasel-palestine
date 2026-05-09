2aca216749f9f00f361d20c7bca95065f5167536
AhmadAdas21 <ahmadnajiadas20@gmail.com>
2026-05-09 09:49:42 +0300

    ....

--- app/models/incident.py
+++ app/models/incident.py
@@ -4,5 +4,13 @@ from sqlalchemy.sql import func
 from app.db.database import Base
 class Incident(Base):
     __tablename__="incidents"
-    id =coloumn(Integer , primary_key=True, index=True)
-    
+    id =Coloumn(Integer , primary_key=True, index=True)
+    title =Coloumn(String,nullable=false)
+    desctiption=Coloumn(String,nullable=true)
+    category=Column(String,nullable=False)
+    severity =Column(String,nullable=False)
+    status=Column(String, default="open")
+    latitude=Column(Float,nullable=False)
+    longitude=Column(Float,nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+  ## created_at=Column(DateTime(timezone=True), server_default=func.now())
---
+++ app/schemas/incident_schema.py
@@ -0,0 +1,27 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+from datetime import datetime
+class IncidentCreate(BaseModel):
+    title:str=Field(...,min_length=3,max_length=100)
+    description:Optional[str] = None
+
+    category:str
+    severity:str
+
+    latitude:float
+    longitude:float
+
+    class IncidentResponse(BaseModel):
+        id :int
+        title :str
+        description:Optional[str] =None
+        category:str
+        severity:str
+        status:str
+        latitude:float
+        longitude:float
+        created_at:datetime
+
+
+    class Config:
+        from_attributes=True
