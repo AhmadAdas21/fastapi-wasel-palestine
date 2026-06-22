1076de7bac3f3c27d9858eb8f35e66bdd1604ce6
AhmadAdas21 <ahmadnajiadas20@gmail.com>
2026-06-22 17:28:12 +0300

    .

--- app/main.py
+++ app/main.py
@@ -11,7 +11,12 @@ title="Wasel Palestine API",
     description="Backend API for Wasel Palestine project",
     version="1.0.0")
 
+ 
 
+
+
+
+ 
 app.include_router(api_router, prefix="/api/v1")
 
 
